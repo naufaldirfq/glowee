64bfce29f5831f1d84d139680d26e1cc263295fc
Naufaldi Athallah Rifqi <naufaldi.athallah@ui.ac.id>
2021-04-01 17:50:29 +0700

    Initial Commit

---
+++ Glowee/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Glowee
+//
+//  Created by Naufaldi Athallah Rifqi on 01/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
