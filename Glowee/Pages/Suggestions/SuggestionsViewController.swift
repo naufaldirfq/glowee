c16150d5e8e3cd27971757bf51617f0a776777b0
Naufaldi Athallah Rifqi <naufaldi.athallah@ui.ac.id>
2021-04-07 17:14:29 +0700

    add tabbar

--- Glowee/Pages/Suggestions/SuggestionsViewController.swift
+++ Glowee/Pages/Suggestions/SuggestionsViewController.swift
@@ -8,12 +8,17 @@
 import UIKit
 
 class SuggestionsViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initView()
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
     }
     
     func initView() {
