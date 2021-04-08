7e85c21d2b25e09d12404ad4a91d5b6b44cce032
ricksen tanata <ricksentanata@gmail.com>
2021-04-08 12:03:27 +0700

    ricksen wibu

--- Glowee/Pages/Reminder/ReminderViewController.swift
+++ Glowee/Pages/Reminder/ReminderViewController.swift
@@ -8,22 +8,37 @@
 import UIKit
 
 class ReminderViewController: UIViewController {
+    
+    @IBOutlet var tableView: UITableView!
+    let names = ["Bambang","Sudirman", "Joko"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+ 
 
-        // Do any additional setup after loading the view.
+}
+
+extension ReminderViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("You Tapped Me")
+    }
+}
+
+extension ReminderViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+ 
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        cell.textLabel?.text = "Hello Pemirsa"
+        
+        return cell
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
