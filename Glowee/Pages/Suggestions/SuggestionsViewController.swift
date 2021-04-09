9803608d81a83aacef9c58afcc2643d5db6a5e94
Naufaldi Athallah Rifqi <naufaldi.athallah@ui.ac.id>
2021-04-09 16:07:06 +0700

    product collectionview

--- Glowee/Pages/Suggestions/SuggestionsViewController.swift
+++ Glowee/Pages/Suggestions/SuggestionsViewController.swift
@@ -7,35 +7,63 @@
 
 import UIKit
 
-class SuggestionsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+class SuggestionsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        2
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-    
-//    @IBOutlet weak var suggestionTableView: UITableView!
+    @IBOutlet weak var suggestionTableView: UITableView!
     @IBOutlet weak var ingredients1Button: UIButton!
     @IBOutlet weak var ingredients2Button: UIButton!
     @IBOutlet weak var ingredients3Button: UIButton!
     @IBOutlet weak var ingredients4Button: UIButton!
     
+    let recommendations = ["Cleanser", "Sunscreen", "Moisturizer"]
+    var productModel = [Model]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        initView()
+        initTableView()
+        initModels()
         // Do any additional setup after loading the view.
     }
     
-    func initView() {
-            
+    func initTableView() {
+        self.suggestionTableView.register(ProductsCollectionViewCell.nib(), forCellReuseIdentifier: ProductsCollectionViewCell.identifier)
+        self.suggestionTableView.delegate = self
+        self.suggestionTableView.dataSource = self
+    }
+    
+    func initModels() {
+        productModel.append(Model(productName: "Garnier", image: "cleanser_1"))
+        productModel.append(Model(productName: "Clean&Clear", image: "cleanser_2"))
+        productModel.append(Model(productName: "Senka", image: "cleanser_3"))
+        productModel.append(Model(productName: "Garnier", image: "cleanser_1"))
+        productModel.append(Model(productName: "Clean&Clear", image: "cleanser_2"))
+        productModel.append(Model(productName: "Senka", image: "cleanser_3"))
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "recommendationTableViewCell", for: indexPath) as? RecommendationTableViewCell {
+            cell.configure(with: productModel)
+            return cell
+        }
+        return UITableViewCell()
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        recommendations.count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return recommendations[section]
     }
     
     
-    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
     
     /*
     // MARK: - Navigation
